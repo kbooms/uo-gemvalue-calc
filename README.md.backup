8093e3b5d3b10ccaf4b5b773ad17cb8946e29c2b
Kevin Booms <mrkbooms@gmail.com>
2022-11-22 23:51:21 -0500

    Version 0.1 Initial Commit

---
+++ README.md.backup
@@ -0,0 +1,19 @@
+# Ultima Online 
+## Gem to Gold Value Calculator
+
+This simple calculator with tally up the total gems in your bag (as user input) and return the current gold value
+
+![gem calculator](uo_gems.png)
+
+> [11/22/22] So far this program is very rudimentary. It runs once, asks the user to input the number of gems in their bags, and the program will calculate the individual and total gold value of each and all gems they possess.
+Future changes will be to convert a lot of the functionality into methods, implement a constructor so a calculator object can be called to run the functions, and eliminate so much of the repetitive code. For the time being the program does what I need it to do.
+
+### Current Version 0.1
+Everything is currently run from one Java Class. User must manually input their own numbers for gems.
+
+**How to run the program**
+1. Download the source code
+2. Open it and compile in your IDE. (This was written using IntelliJ)
+3. Follow the input prompts to see the value of your stashed gems
+
+Thank you for viewing!
---
+++ src/main/java/com/kevinbooms/GemValueCalc.java
@@ -0,0 +1,89 @@
+package com.kevinbooms;
+
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+
+    /*
+    * So far this program is very rudimentary. It runs once, asks the user to input the number of gems
+    * in their bags, adn the program will calculate the individual and total gold value of each and all
+    * gems they possess.
+    *
+    * Future changes will be to convert a lot of the functions into methods, implement a constructor
+    * so a calculator object can be called to run the functions, for the time being the program does
+    * what I need it to do.
+    */
+
+public class GemValueCalc {
+
+    public static void main(String[] args) {
+
+        int citrine = 0;
+        int tourmaline = 0;
+        int amber = 0;
+        int amethyst = 0;
+        int ruby = 0;
+        int sapphire = 0;
+        int emerald = 0;
+        int starSapphire =  0;
+        int diamond = 0;
+
+        final int CITRINE_VALUE = 20;
+        final int TOURMALINE_VALUE = 30;
+        final int AMBER_VALUE = 40;
+        final int AMETHYST_VALUE = 50;
+        final int RUBY_VALUE = 60;
+        final int SAPPHIRE_VALUE = 70;
+        final int EMERALD_VALUE = 80;
+        final int STAR_SAPPHIRE_VALUE = 90;
+        final int DIAMOND_VALUE = 100;
+
+        System.out.println("This simple calculator will add the total amount of gems you enter (representing your current bag) " +
+                "and calculate the vendor gold value of the gems you've entered. \n");
+        System.out.println("Please enter the amount of gems: ");
+        Scanner gemCount = new Scanner(System.in);
+        System.out.print("Citrines? ");
+        citrine = parseInt(gemCount.next()); // 74
+        System.out.print("Tourmalines? ");
+        tourmaline = parseInt(gemCount.next()); // 51
+        System.out.print("Ambers? ");
+        amber = parseInt(gemCount.next()); // 44
+        System.out.print("Amethysts? ");
+        amethyst = parseInt(gemCount.next()); // 51
+        System.out.print("Rubies? ");
+        ruby = parseInt(gemCount.next()); // 31
+        System.out.print("Sapphires? ");
+        sapphire = parseInt(gemCount.next()); // 31
+        System.out.print("Emeralds? ");
+        emerald = parseInt(gemCount.next()); // 14
+        System.out.print("Star Sapphires? ");
+        starSapphire = parseInt(gemCount.next()); // 31
+        System.out.print("Diamonds? ");
+        diamond = parseInt(gemCount.next()); // 9
+
+        int totalValue = ((CITRINE_VALUE * citrine) +
+                (TOURMALINE_VALUE * tourmaline) +
+                (AMBER_VALUE * amber) +
+                (AMETHYST_VALUE * amethyst) +
+                (RUBY_VALUE * ruby) +
+                (SAPPHIRE_VALUE * sapphire) +
+                (EMERALD_VALUE * emerald) +
+                (STAR_SAPPHIRE_VALUE * starSapphire) +
+                (DIAMOND_VALUE * diamond));
+
+        System.out.println("Citrines:       " + citrine + " @ " + CITRINE_VALUE + " gp = " + (CITRINE_VALUE * citrine) + "gp");
+        System.out.println("Tourmalines:    " + tourmaline + " @ " + TOURMALINE_VALUE + " gp = " + (TOURMALINE_VALUE * tourmaline) + "gp");
+        System.out.println("Amber:          " + amber + " @ " + AMBER_VALUE + " gp = " + (AMBER_VALUE * amber) + "gp");
+        System.out.println("Amethysts:      " + amethyst + " @ " + AMETHYST_VALUE + " gp = " + (AMETHYST_VALUE * amethyst) + "gp");
+        System.out.println("Rubies:         " + ruby + " @ " + RUBY_VALUE + " gp = " + (RUBY_VALUE * ruby) + "gp");
+        System.out.println("Sapphires:      " + sapphire + " @ " + SAPPHIRE_VALUE + " gp = " + (SAPPHIRE_VALUE * sapphire) + "gp");
+        System.out.println("Emeralds:       " + emerald + " @ " + EMERALD_VALUE + " gp = " + (EMERALD_VALUE * emerald) + "gp");
+        System.out.println("Star Sapphires: " + starSapphire + " @ " + STAR_SAPPHIRE_VALUE + " gp = " + (STAR_SAPPHIRE_VALUE * starSapphire) + "gp");
+        System.out.println("Diamonds:       " + diamond +  " @ " + DIAMOND_VALUE + " gp = " + (DIAMOND_VALUE * diamond) + "gp" + "\n");
+
+        System.out.println("Total Gold Value: " + totalValue);
+    }
+
+
+}
+
